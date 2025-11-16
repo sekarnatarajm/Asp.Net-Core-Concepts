6b12f837ab6440bd02fe7df436a8ce061e7c82d2
Sekar_Nataraj <Sekar.Nataraj@dellteam.com>
2025-11-16 23:25:41 +0530

    JWT base setup completed.

---
+++ AspNetCoreLearning.JwtAuthentication/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AspNetCoreLearning.JwtAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController(IConfiguration configuration) : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            string jsonToken = "";
+            if (user is null)
+            {
+                return BadRequest("User object null");
+            }
+            if (!string.IsNullOrEmpty(user.UserName) && !string.IsNullOrEmpty(user.Password))
+            {
+                var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345-superSecretKey@345-superSecretKey@345"));
+                var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+                var tokeOptions = new JwtSecurityToken(
+                    issuer: "https://localhost:5001",
+                    audience: "https://localhost:5001",
+                    claims: new List<Claim>(),
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: signinCredentials
+                );
+                jsonToken = new JwtSecurityTokenHandler().WriteToken(tokeOptions);
+            }
+            return Ok(new AuthenticatedResponse { Token = jsonToken });
+        }
+    }
+
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+    internal class AuthenticatedResponse
+    {
+        public string Token { get; set; }
+    }
+}
