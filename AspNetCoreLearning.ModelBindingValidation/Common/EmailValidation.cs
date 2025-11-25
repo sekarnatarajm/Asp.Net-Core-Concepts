990d308b53243cd305df5bef360884e457facdce
Sekar_Nataraj <Sekar.Nataraj@dellteam.com>
2025-11-25 23:31:24 +0530

    Custom validation implemented

---
+++ AspNetCoreLearning.ModelBindingValidation/Common/EmailValidation.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AspNetCoreLearning.ModelBindingValidation.Common
+{
+    public static class EmailValidation
+    {
+        public static bool EmailDomainValidate(string email, List<string> domains)
+        {
+            string domainPattern = string.Join("|", domains.ConvertAll(d => Regex.Escape(d.ToLower())));
+            string blockedDomainsPattern = $@"^[^@]+@({domainPattern})$";
+            bool resukt = Regex.IsMatch(email, blockedDomainsPattern, RegexOptions.IgnoreCase);
+            return resukt;
+        }
+    }
+}
---
+++ AspNetCoreLearning.ModelBindingValidation/Common/FutureDateAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AspNetCoreLearning.ModelBindingValidation.Common
+{
+    public class FutureDateAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object? value)
+        {
+            if (value is DateTime date)
+            {
+                return date > DateTime.Now;
+            }
+            return true;
+        }
+    }
+}
